717a1210c5a8604cd0e24dac95973531da847239
Guilherme Moreira <gui.moreirasa@gmail.com>
2023-07-08 18:30:37 -0300

    removing constant files

--- src/main/java/app/moogui/constants/SecurityConstants.java
+++
@@ -1,6 +0,0 @@
-package app.moogui.constants;
-
-public class SecurityConstants {
-	public static final String JWT_KEY = "jxgEQeXHuJJKnjgy756udQ53YUn4";
-    public static final String JWT_HEADER = "Authorization";
-}
