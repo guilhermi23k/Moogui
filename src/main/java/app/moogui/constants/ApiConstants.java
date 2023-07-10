f6aa232b7757656867d7d473cad0f0d348add5db
Guilherme Moreira <gui.moreirasa@gmail.com>
2023-07-10 15:03:47 -0300

    removing sensive files

--- src/main/java/app/moogui/constants/ApiConstants.java
+++
@@ -1,7 +0,0 @@
-package app.moogui.constants;
-
-public class ApiConstants {
-	public static final String gpt_key = "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";
-	public static final String omdb_key = "teste";
-}
- 
